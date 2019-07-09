7f29604b5b53dbe5af9899ed487f4e04592d52a4
vengle <vernon.g.engle@accenture.com>
2019-07-10 09:52:00 +1000

    test CORS

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
                     <div className="ui card">
                         <div className="content">
                             <a className="header">Player Red</a>
-                            <p>{this.state.question}</p>
+                            <p>{this.state.question[0]}</p>
                         </div>
                     </div>
                 </div>
